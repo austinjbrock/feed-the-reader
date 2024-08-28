9371385d1670797feff4df9806ce15aef4c7e43d
Austin Brock <austinbrock@Austins-MacBook.local>
2024-08-27 22:04:24 -0700

    working app

---
+++ feeder.rb
@@ -0,0 +1,20 @@
+require 'rss'
+require 'open-uri'
+
+# URL of the RSS feed
+rss_url = 'https://www.chasingthedonkey.com/croatia-travel-blog/feed/'
+
+  content = URI.open(rss_url).read
+
+  # Parse the RSS feed
+  feed = RSS::Parser.parse(content, false)
+
+  # Iterate through each item in the RSS feed
+  feed.items.each do |item|
+    puts "Title: #{item.title}"
+    puts "Link: #{item.link}"
+    puts "Description: #{item.description}"
+    puts "PubDate: #{item.pubDate}"
+    #Separate each article by dashes
+    puts "---" * 10 
+  end
